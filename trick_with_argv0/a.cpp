620b4e4cfe83edd4ab3ba27ce54eedf48ddd8d43
Arthur Besse <arthurcaturian@gmail.com>
2021-11-19 23:13:33 +0400

    Added loger logic, also added fork call before exec

--- trick_with_argv0/a.cpp
+++ trick_with_argv0/a.cpp
@@ -11,26 +11,38 @@
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <sys/wait.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <libgen.h>
-
+#include <logger.h>
 
 int main (int argc, char ** argv)
 {
+	Logger logger(NULL);
+
 	if(2 != argc)
 	{
-		fprintf(stderr, "Error: Invalid arguments\n");
+		Logger::logf(Logger::ERROR, __FILE__, __LINE__, "Error: Invalid arguments\n");
 		exit(EXIT_FAILURE);
-	} 
+	}
 
-	char * args[] = { argv[1], (char*)NULL }; //Удивительный факт: если не написать NULL в конце и запускать под Windows 
-						  //то Windows падает с синим экраном :)
-	if(0 > execv("./b", args))
+	char * args[] = { argv[1], (char*)NULL };
+	int pid = fork();
+
+	if(0 > pid)
+	{
+		perror(NULL);
+		exit(EXIT_FAILURE);
+	}
+
+	if(0 == pid && 0 > execv("./b", args))
 	{
 		perror("Error occurred while calling b");
 		exit(EXIT_FAILURE);
 	}
+	else
+		wait(NULL);
 
 	exit(EXIT_SUCCESS);
 }
