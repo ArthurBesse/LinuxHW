d30ecab2bca6ce77e549b99a5c7de8c7a6f84ef6
Arthur Besse <arthurcaturian@gmail.com>
2021-11-19 23:24:33 +0400

    Added fork before exec

--- ls_a_l/ls_a_l.cpp
+++ ls_a_l/ls_a_l.cpp
@@ -1,15 +1,39 @@
-#include <unistd.h>
-#include <stdlib.h>
+#include <chrono>
+#include <string>
+#include <iostream>
+#include <vector>
+#include <sstream>
 #include <stdio.h>
+#include <strings.h>
+#include <string.h>
 #include <errno.h>
+#include <ftw.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <libgen.h>
 
 
 int main(int argc, char const ** argv)
 {
-	if(0 > execlp("ls", "-a", "-l", (char*)NULL))
+	int pid = fork();
+	if(0 > pid)
 	{
 		perror(NULL);
 		exit(EXIT_FAILURE);
 	}
+	if(0 == pid)
+	{
+		if(0 > execlp("ls", "-a", "-l", (char*)NULL))
+		{
+			perror(NULL);
+			exit(EXIT_FAILURE);
+		}
+	}
+	else
+		wait(NULL);
 	exit(EXIT_SUCCESS);
 }
