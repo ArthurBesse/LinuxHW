72d206f6bde5dd0c3290167b27162f50a1567b30
Arthur <arthurcaturian@gmail.com>
2021-12-28 03:44:25 +0400

    Logger update

--- logger.h
+++ logger.h
@@ -1,67 +1,86 @@
+#include <iomanip>
+#include <ctime>
 #include <iostream>
 #include <sstream>
-#include <initializer_list>
-#include <boost/log/core.hpp>
-#include <boost/log/trivial.hpp>
-#include <boost/log/expressions.hpp>
-#include <boost/log/utility/setup/file.hpp>
-#include <boost/log/utility/setup/common_attributes.hpp>
+#include <chrono>
+#include <fstream>
 
 
 class Logger
 {
 private:
-	static inline Logger* this_ptr = NULL;
+
+        static inline Logger* this_ptr = NULL;
+        std::ofstream outf;
+        std::ostream& out;
 public:
-	Logger(char const * filename)
-	{
-		if(NULL != this_ptr)
-			throw std::runtime_error("Only one instance of Logger can be created");
-		this_ptr = this;
-		if(NULL != filename)
-			boost::log::keywords::file_name = filename;
-		boost::log::keywords::format = "[%TimeStamp%] [%SEVERITY%]: %Message%";
-	}
-
-	~Logger()
-	{
-		this_ptr = NULL;
-	}
-
-	enum severity_t{ INFO, WARNING, ERROR, FATAL, DEBUG };
-	
-	template<typename ...T>
-	static void log(Logger::severity_t severity, T&& ... args)
-	{
-		if(NULL == this_ptr)
-			throw std::runtime_error("The logger must first be instantiated");
-		
-		std::stringstream message_constructor;
-		
-		std::initializer_list<int> temp{(message_constructor << std::forward<T>(args), 0) ... };
-
-		switch(severity)
-		{
-		case INFO:
-			BOOST_LOG_TRIVIAL(info) << message_constructor.str();
-			break;
-		case WARNING:
-                        BOOST_LOG_TRIVIAL(warning) << message_constructor.str();
-			break;
-		case ERROR:
-                        BOOST_LOG_TRIVIAL(error) << message_constructor.str();
-			break;
-		case FATAL:
-                        BOOST_LOG_TRIVIAL(fatal) << message_constructor.str();
-			break;
-		case DEBUG:
-			BOOST_LOG_TRIVIAL(debug) << message_constructor.str();
-		}
-	}
-	template<typename ...T>
-	static void logf(Logger::severity_t severity, const char * FILE, size_t LINE, T&& ... args)
+        Logger(char const * filename)
+                : outf(filename)
+                , out(filename ?
+                        outf : std::cout)
         {
-		log(severity, "In ", FILE, ":", LINE, " ", std::forward<T>(args) ...); 
+                if(NULL != this_ptr)
+                        throw std::runtime_error("Only one instance of Logger can be created");
+                this_ptr = this;
+        }
+
+        ~Logger()
+        {
+                outf.close();
+                this_ptr = NULL;
+        }
+
+        enum severity_t{ INFO, WARNING, ERROR, FATAL, DEBUG };
+
+
+        template<typename ...T>
+        static void logf(Logger::severity_t severity, const char * FILE, size_t LINE, T&& ... args) //__attribute__ ((format(printf, 4, 5)))
+        {
+                if(nullptr == this_ptr)
+                        throw std::runtime_error("The logger must first be instantiated");
+                this_ptr->logf_internal(severity, FILE, LINE, args ...);
+        }
+
+private:
+
+        char const * get_severity_str(Logger::severity_t severity)
+        {
+                switch(severity)
+                {
+                case INFO:
+                        return "INFO";
+                        break;
+                case WARNING:
+                        return "WARNING";
+                        break;
+                case ERROR:
+                        return "ERROR";
+                        break;                
+				case FATAL:
+                        return "FATAL";
+                        break;
+                case DEBUG:
+                        return "DEBUG";
+                }
+                return nullptr;
+        }
+        template<typename ...T>
+        void logf_internal(Logger::severity_t severity, const char * FILE, size_t LINE, char const * fmt, T&& ... args)
+        {
+                std::time_t cur_time = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+                std::tm * cur_time_local = std::localtime(&cur_time);
+                out << std::put_time(cur_time_local, "%Y%m%d%H%M%S") << " " << get_severity_str(severity) << ": " << this->format(fmt, std::forward<T>(args)...);
+        }
+
+        template<typename ... Args>
+        std::string format( char const * format, Args ... args )
+        {
+            int size_s = std::snprintf( nullptr, 0, format, args ... ) + 1; // Extra space for '\0'
+            if( size_s <= 0 ){ throw std::runtime_error( "Error during formatting." ); }
+            size_t size = static_cast<size_t>( size_s );
+            auto buf = std::make_unique<char[]>( size );
+            std::snprintf( buf.get(), size, format, args ... );
+            return std::string( buf.get(), buf.get() + size - 1 ); // We don't want the '\0' inside
         }
 
 };
